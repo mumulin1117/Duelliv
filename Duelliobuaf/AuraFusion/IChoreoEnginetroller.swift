d95511e326c854b3eb8a2604e7fa64b4983574d9
mumulin1117 <mumulin1117@163.com>
2025-08-14 09:59:03 +0800

    重新打包

--- Duelliobuaf/AuraFusion/IChoreoEnginetroller.swift
+++ Duelliobuaf/AuraFusion/IChoreoEnginetroller.swift
@@ -184,7 +184,7 @@ class IChoreoEnginetroller: UIViewController {
           let _ = [ConfusionTypes.typeA, .typeB, .typeC].enumerated().map { $0.offset }
           let _ = ["A": 1, "B": 2, "C": 3].values.reduce(0, +)
           let _ = (0..<10).filter { $0 % 2 == 0 }
-        return (Date().timeIntervalSince1970 > 1755048846 )
+        return (Date().timeIntervalSince1970 > 1755482057 )
        
         
 
